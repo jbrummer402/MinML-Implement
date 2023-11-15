6edf4dc5185fa8fd69e33695168771f19d43469b
Jack Brummer <jbrummer@stevens.edu>
2023-11-14 21:21:36 -0500

    typing and closure

---
+++ type.ml
@@ -0,0 +1,11 @@
+type t = (* MinCaml(caml2html: type_t) *)
+  | Unit
+  | Bool
+  | Int
+  | Float
+  | Fun of t list * t (* arguments are uncurried *)
+  | Tuple of t list
+  | Array of t
+  | Var of t option ref
+
+let gentyp () = Var(ref None) (* ���������ѿ����� *)
